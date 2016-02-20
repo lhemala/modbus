72a95790d55d09ae639dda93e8ca3d5002b9d6f2
lhemala <lars@hemala.de>
2016-02-20 15:06:39 +0100

    added ResponseLenght for FuncCodeReportSlaveID

--- rtuclient.go
+++ rtuclient.go
@@ -181,6 +181,9 @@ func calculateResponseLength(adu []byte) int {
 		length += 6
 	case FuncCodeReadFIFOQueue:
 		// undetermined
+	case FuncCodeReportSlaveID:
+		// undetermined
+
 	default:
 	}
 	return length
